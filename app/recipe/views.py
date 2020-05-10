6b83abf8e29a66b567877ea89fe635df19dc0b9d
Mattia <mattiapos@gmail.com>
2020-05-10 11:49:04 +0200

    Added recipe image upload feature.

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,6 +1,8 @@
-from rest_framework import mixins, viewsets
+from rest_framework import mixins, status, viewsets
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from core.models import Ingredient, Recipe, Tag
 from recipe import serializers
@@ -65,6 +67,10 @@ class RecipeViewSet(viewsets.ModelViewSet):
         """
         if self.action == 'retrieve':
             return serializers.RecipeDetailSerializer
+        elif self.action == 'upload_image':
+            # N.B.: here the action name is the same of the
+            # method 'upload_image' defined below, not the URL.
+            return serializers.RecipeImageSerializer
 
         return self.serializer_class
 
@@ -73,3 +79,25 @@ class RecipeViewSet(viewsets.ModelViewSet):
         Create a new recipe.
         """
         serializer.save(user=self.request.user)
+
+    @action(methods=['POST'], detail=True, url_path='upload-image')
+    def upload_image(self, request, pk=None):
+        """
+        Upload an image to a recipe.
+        """
+        recipe = self.get_object()
+        serializer = self.get_serializer(
+            recipe,
+            data=request.data
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                serializer.data,
+                status=status.HTTP_200_OK
+            )
+
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
